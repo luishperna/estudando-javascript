0d3cf85faa736d4bcd8ab561e55f673f15b6e4e2
Luís <luishperna@gmail.com>
2022-09-25 23:34:51 -0300

    Padronizando diretório

---
+++ function.js
@@ -0,0 +1,8 @@
+// Declarando uma função com o nome de tocaSomPom
+function tocaSomPom() {
+    // Acessando um elemento e chamando a função play() para iniciar um audio
+    document.querySelector('#som_tecla_pom').play();
+    }
+
+// Chamando essa função
+tocaSomPom()
---
+++ querySelector.js
@@ -0,0 +1,20 @@
+// Função para buscar um elemento no documento HTML
+document.querySelector();
+
+// ... buscando pela tag 
+document.querySelector('input');
+
+// ... buscando pelo identificador 
+document.querySelector('#id-input');
+
+// ... buscando pela classe 
+document.querySelector('.class-input');
+
+// ... buscando pelo elemento (tag) 
+document.querySelector('input');
+
+// ... buscando pela tag input que tem o type=tel
+document.querySelector('input[type=tel]');
+
+// Acessando um elemento e chamando a função play() para iniciar um audio
+document.querySelector('#som').play();
